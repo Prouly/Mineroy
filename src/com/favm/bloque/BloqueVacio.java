64aa3efa6d4bc13fa88b6b1ab2728b7c289f7470
Fran Villa <80397839+FranVilla90@users.noreply.github.com>
2022-04-03 14:32:59 +0200

    Update

---
+++ src/com/favm/bloque/BloqueVacio.java
@@ -0,0 +1,16 @@
+package bloque;
+
+
+public class BloqueVacio extends Bloque {
+
+	public BloqueVacio(int x, int y, int z) throws MineroyException {
+		super(x, y, z);
+		
+	}
+	
+	@Override
+	public String toString() {
+		return super.toString()+", Bloque vacio";
+	}
+
+}
---
+++ src/com/favm/bloque/MineroyException.java
@@ -0,0 +1,13 @@
+package bloque;
+
+public class MineroyException extends Exception{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public MineroyException (String msg) {
+		super(msg);
+	}
+
+}
