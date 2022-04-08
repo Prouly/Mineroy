e43a67ad80edbadeaaab34514e0d017f51f1a19a
Fran Villa <80397839+FranVilla90@users.noreply.github.com>
2022-04-08 09:22:50 +0200

    Update

---
+++ src/com/juego/categoria/BloqueTierra.java
@@ -0,0 +1,16 @@
+package categoria;
+
+import juego.Jugador;
+
+/**
+ * Bloque tierra
+ * 
+ * @author Alvaro M. & Fran V.
+ *
+ */
+
+public interface BloqueTierra {
+	public static final String HERRAMIENTA ="Pala";
+	
+	public void destruir (String herramienta, Jugador jugador);
+}
