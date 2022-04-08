921562c5f59872657c014eb50ff97b548bdc6f1d
Fran Villa <80397839+FranVilla90@users.noreply.github.com>
2022-04-08 09:21:41 +0200

    Update

---
+++ src/bloque/BloqueHierro.java
@@ -0,0 +1,30 @@
+package bloque;
+
+import bloque.categoria.BloqueMineral;
+import juego.Jugador;
+
+public class BloqueHierro extends Bloque implements BloqueMineral{
+	
+	private int material;
+	
+	public BloqueHierro(int x, int y, int z) throws MineroyException {
+		super(x, y, z);
+		this.material = Bloque.HIERRO;
+	}
+	
+	public void destruir (String herramienta, Jugador jugador) {
+		if (!(herramienta.equalsIgnoreCase(HERRAMIENTA))) {
+			super.destruir(herramienta, jugador);
+		} else {
+			super.destruir(herramienta, jugador);	 
+			jugador.sumaMateria(material);
+		}
+		
+	}
+	
+	@Override
+	public String toString() {
+		return super.toString()+", material Bloque Hierro: " + material;
+	}
+
+}
---
+++ src/bloque/categoria/BloqueVegetal.java
@@ -0,0 +1,16 @@
+package bloque.categoria;
+
+import juego.Jugador;
+
+/**
+ * Bloque vegetal
+ * 
+ * @author Alvaro M. & Fran V.
+ *
+ */
+
+public interface BloqueVegetal {
+	public static final String HERRAMIENTA ="Hacha";
+	
+	public void destruir (String herramienta, Jugador jugador);
+}
